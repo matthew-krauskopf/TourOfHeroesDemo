3e0a678c5b8141b7e8d5f80acd72a0f3f8a04651
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2021-01-26 16:41:48 -0500

    Step 6.1: Add CRUD Operations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service'; 
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -20,7 +24,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,24 +3,70 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { ThisReceiver } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  }
+
   getHeroes() : Observable<Hero[]> {
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+              .pipe(tap(_ => this.log('fetched heroes')), 
+                    catchError(this.handleError<Hero[]>('getHeroes', []))
+                    );
   }
 
   getHero(id: number) : Observable<Hero> {
-    this.messageService.add(`Hero being fetched: id=${id}`);
-    const found = HEROES.find(hero => hero.id === id);
-    if (found === undefined) throw new TypeError("Not possible!");
-    return of(found);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url)
+            .pipe(tap(_ => this.log(`Hero being fetched: id=${id}`)),
+            catchError(this.handleError<Hero>(`getHero id=${id}`))
+            );
   }
 
-  constructor(private messageService : MessageService) { 
+  constructor(private messageService : MessageService, private http : HttpClient) { 
 
+  }
+
+  private log(message: string) : void {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log error
+      this.log(`${operation} failed: ${error.message}`); // log error for user consumption
+      return of(result as T); // let app keep running
+    };
+  }
+
+  updateHero(hero: Hero) : Observable<any> {
+    return this.http.put<any>(this.heroesUrl, hero, this.httpOptions)
+            .pipe(tap(_ => this.log(`Hero being updated: id=${hero.id}`)),
+            catchError(this.handleError<any>(`updatehero id=${hero.id}`))
+            );
+  }
+
+  addHero(hero: Hero) : Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+            .pipe(tap((newHero: Hero) => this.log(`Hero added: id=${hero.id}`)),
+            catchError(this.handleError<Hero>(`AddHero error: id=${hero.id}`))
+            );
+  }
+
+  deleteHero(hero: Hero) : Observable<Hero> {   
+    return this.http.delete<Hero>(`${this.heroesUrl}/${hero.id}`, this.httpOptions)
+            .pipe(tap(_ => this.log(`deleted hero ${hero.name}`)),
+            catchError(this.handleError<Hero>(`deleteHero: name = ${hero.name}`))
+            );
   }
 }
