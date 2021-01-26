974d7eae45238a8717b17aabdac1b27c35b2dd91
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2021-01-26 17:09:12 -0500

    Step 6.2: Add search field

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -69,4 +69,12 @@ export class HeroService {
             catchError(this.handleError<Hero>(`deleteHero: name = ${hero.name}`))
             );
   }
+
+  searchHeroes(term: string) : Observable<Hero[]> {
+    if (!term.trim) return of([]); // returns nothing if search field empty
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+        tap(x => x.length ? this.log(`found heroes matching ${term}`) : this.log(`found heroes matching ${term}`)),
+        catchError(this.handleError<Hero[]>('searchHeroes', []))
+        );
+  }
 }
